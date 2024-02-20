1f3efdbef91bd54a54714f2c228ca407d1cd7176
Pankaj Kumar <pankajamsali@gmail.com>
2024-02-20 22:26:23 +0530

    bgChanger

--- 03tailwindprops/src/components/Cards.jsx
+++ 03tailwindprops/src/components/Cards.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Cards() {
+function Cards({username,btnText="Click Here"}) {
   return (
     <div className="relative h-[400px] w-[300px] rounded-md mt-4">
     <img
@@ -10,13 +10,13 @@ function Cards() {
     />
     <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
     <div className="absolute bottom-4 left-4 text-left">
-      <h1 className="text-lg font-semibold text-white">Delba</h1>
+      <h1 className="text-lg font-semibold text-white">{username}</h1>
       <p className="mt-2 text-sm text-gray-300">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
         debitis?
       </p>
       <button className="mt-4 inline-flex cursor-pointer items-center text-sm font-semibold text-black-400">
-        View Profile →
+        {btnText}
       </button>
     </div>
   </div>
