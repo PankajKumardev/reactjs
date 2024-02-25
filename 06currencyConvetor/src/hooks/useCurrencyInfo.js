3e78375796d857eb186e53231092513831acd6a6
Pankaj Kumar <pankajamsali@gmail.com>
2024-02-25 12:04:49 +0530

    yes

--- 06currencyConvetor/src/hooks/useCurrencyInfo.js
+++ 06currencyConvetor/src/hooks/useCurrencyInfo.js
@@ -1,5 +1,8 @@
 import {useEffect,useState} from 'react'
 
 function useCurrencyInfo() {
-    useEffect(()=>{},[])
+    useEffect(()=>{
+        let url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+        fetch(url)
+    },[])
 }
